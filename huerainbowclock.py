c757f58c70cc0e02c9521c573e368e6487a3437e
Johann Weiher <info@suplanus.de>
2016-10-12 10:01:12 +0200

    Color to the clock 🌈

--- huerainbowclock.py
+++ huerainbowclock.py
@@ -11,22 +11,63 @@ from phue import Bridge
 import colorama
 from colorama import Fore, Back, Style
 
+from rgb_cie import Converter
+
 HUEBRIDGEIP = "192.168.178.79"
 LIGHTNAME = "Buro"
+
+LAMP = ""
 
 # Executes hour changed
 def DingDong():
 	global hourBefore
 
-	# Set lamp
-	Bridge.set_light(LIGHTNAME,'bri', 254) # turn light on
+	# Turn lamp on
+	LAMP.brightness = 254
 
-	# Calc the saturation https://en.wikipedia.org/wiki/Hue
-	saturation = int(round((254/24) * hour))
-	Bridge.set_light(LIGHTNAME,'sat', saturation) # turn light on
+	# Change color
+	converter = Converter()
+
+	if hour == 1 or hour == 13:
+		xy = converter.rgbToCIE1931(255,0,0)
+
+	if hour == 2 or hour == 14:
+		xy = converter.rgbToCIE1931(255,128,0)
+
+	if hour == 3 or hour == 15:
+		xy = converter.rgbToCIE1931(255,255,0)
+
+	if hour == 4 or hour == 16:
+		xy = converter.rgbToCIE1931(128,255,0)
+
+	if hour == 5 or hour == 17:
+		xy = converter.rgbToCIE1931(0,255,0)
+
+	if hour == 6 or hour == 18:
+		xy = converter.rgbToCIE1931(0,255,128)
+
+	if hour == 7 or hour == 19:
+		xy = converter.rgbToCIE1931(0,255,255)
+
+	if hour == 8 or hour == 20:
+		xy = converter.rgbToCIE1931(0,128,255)
+
+	if hour == 9 or hour == 21:
+		xy = converter.rgbToCIE1931(0,0,255)
+
+	if hour == 10 or hour == 22:
+		xy = converter.rgbToCIE1931(128,0,255)
+
+	if hour == 11 or hour == 23:
+		xy = converter.rgbToCIE1931(255,0,255)
+
+	if hour == 12 or hour == 24:
+		xy = converter.rgbToCIE1931(255,0,128)
+
+	LAMP.xy = xy
 
 	# Log
-	text = "==> " + str(hour) + " ==> " + str(saturation)
+	text = "==> " + str(hour) + " ==> " + str(xy)
 	print(Fore.LIGHTBLUE_EX + text + Style.RESET_ALL)
 
 	# Save for check later
@@ -41,8 +82,8 @@ Bridge.get_api() # Get the bridge state (This returns the full dictionary that y
 # Get a dictionary with the light name as the key
 light_names = Bridge.get_light_objects('name')
 
-# Set the birghtness of the bulb named "Kitchen"
-light_names[LIGHTNAME].brightness = 100
+# Get light object
+LAMP = light_names[LIGHTNAME]
 
 # Do it to the end of time
 print(Fore.LIGHTBLUE_EX + 'Read time' + Style.RESET_ALL)
@@ -53,13 +94,15 @@ while(True):
 	now = datetime.datetime.now()
 	hour = int(now.strftime("%H"))
 
+	# Test
+	#for i in range(1,12):
+	#	hour = i
+	#	DingDong()
+	#	time.sleep(2)
+
 	# Check if already set
 	if hour == hourBefore:
 		continue # do nothing
 
-	# Only display from 7am to 6pm
-	if hour >= 7 or hour <= 18:
-		DingDong()
-	else:
-		print("==> " + str(hour) + " ==> Sleeping")
-		Bridge.set_light(LIGHTNAME,'on', False) # turn light off
+	# Doit
+	DingDong()
